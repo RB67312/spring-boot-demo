5310e44718987dae88d1668111bf406dd71582b8
RB67312 <105401437+RB67312@users.noreply.github.com>
2022-06-20 13:21:33 +0530

    Add files via upload

---
+++ spring boot product assignment/src/main/java/com/zensar/Repository/ProductRepository.java
@@ -0,0 +1,33 @@
+package com.zensar.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.zensar.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+	
+	@Query("from Product p where  p.productName=?1 or p.productPrice=?2")
+	List<Product> repoQuery1(int ProductId, int poductprice);
+
+	
+	List<Product> test(String productName);
+
+	List<Product> test1(String productName, int productPrice);
+
+	List<Product> test3(String productName, int productPrice);
+
+	@Query("from Product p where p.productName=:name and p.productPrice=:price")
+	List<Product> testt(@Param("name") String productName, @Param("price") int price);
+	
+	
+	
+	
+}
+
+
+
---
+++ spring boot product assignment/src/main/java/com/zensar/SpringBootProductApplication.java
@@ -0,0 +1,30 @@
+package com.zensar;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "test", "com.zensar" })
+public class SpringBootProductApplication {
+	//public class SpringBootProductApplication extends SpringBootServletInitializer{
+
+	public static void main(String... args) {
+		System.out.println("Raghav");
+		SpringApplication.run(SpringBootProductApplication.class, args);
+		System.out.println("Battula");
+	}
+	//@Override
+		 
+		@Bean
+		public ModelMapper getmodelMapper() {
+			return new ModelMapper();
+
+		}
+
+	}
+
+
+
