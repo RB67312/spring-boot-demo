3abe5d7fdffa687eb3ee44e38938260f38ba287a
RB67312 <105401437+RB67312@users.noreply.github.com>
2022-06-24 11:56:39 +0530

    Add files via upload

---
+++ coupon assignment 23.6.22/src/main/java/com/zensar/SpringBootCouponApplication.java
@@ -0,0 +1,26 @@
+package com.zensar;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.zensar" })
+public class SpringBootCouponApplication {
+	// public class SpringBootProjectApplication extends SpringBootServletInitializer
+
+	public static void main(String[] args) {
+		System.out.println("I am Raghavendra");
+		SpringApplication.run(SpringBootCouponApplication.class, args);
+		System.out.println("Battula");
+	}
+
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
