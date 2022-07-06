4b4af2294de1007f1082023865255758272b837e
RB67312 <105401437+RB67312@users.noreply.github.com>
2022-07-06 09:34:53 +0530

    Add files via upload

---
+++ springbootDemo(sonar)/src/main/java/com/zensar/springbootDemo/SpringbootDemoApplication.java
@@ -0,0 +1,26 @@
+package com.zensar.springbootDemo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"test","com.zensar"})
+@OpenAPIDefinition
+//public class SpringbootDemoApplication extends SpringBootServletInitializer{
+public class SpringbootDemoApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+	
+}
